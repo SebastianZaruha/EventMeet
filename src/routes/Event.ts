d621f29491684802d91eca202bb7413244efef65
Jesus Repolles <jesusrepolles@gmail.com>
2025-02-20 18:14:50 +0100

    Funciones para publicar eventos

--- src/routes/Event.ts
+++ src/routes/Event.ts
@@ -15,7 +15,7 @@ const router = Router();
 router.get("/event/:id", authMiddleware, getEvent);
 router.get("/events", getEvents);
 router.get("/events/by-interest/:tag", authMiddleware, getEventsByInterestTag);
-router.post("/event", authMiddleware, postEvent);
+router.post("/event",  postEvent);
 router.put("/event/:id", authMiddleware, updateEvent);
 router.delete("/event/:id", authMiddleware, deleteEvent);
 router.post("/events/filter", authMiddleware, getFilteredEvents);
